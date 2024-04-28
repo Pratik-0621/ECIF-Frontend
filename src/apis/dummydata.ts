f827e9b7d74dd5caaa60138c758584873696e628
Pratik Kumar <pratikkumar@Pratiks-MacBook-Air.local>
2024-04-28 22:44:02 +0530

    Added libraries and removed template files

---
+++ src/apis/dummydata.ts
@@ -0,0 +1,38 @@
+export const dummyData = {
+  id: "7737d7a0-8d9f-44b3-ba52-f93b29432dd3",
+  NAME: "mysql-gods-prd-weeu-01/d18171ac-1488-42c0-b2ce-b94bf7dcf000",
+  ALERTID:
+    "/subscriptions/315a8af3-1c67-4bf3-9f56-08d34b7133da/resourcegroups/rg-gods-prd-weeu-01/providers/microsoft.dbformysql/flexibleservers/mysql-gods-prd-weeu-01/providers/Microsoft.AlertsManagement/alerts/d18171ac-1488-42c0-b2ce-b94bf7dcf000",
+  SEVERITY: "Sev2",
+  ESSENTIALS: {
+    lastModifiedDateTime: "2024-03-29T14:21:57.406Z",
+    lastModifiedUserName: "System",
+    targetResourceGroup: "rg-gods-prd-weeu-01",
+    targetResourceName: "mysql-gods-prd-weeu-01",
+    targetResourceType: "microsoft.dbformysql/flexibleservers",
+    monitorCondition: "Fired",
+    sourceCreatedId:
+      "315a8af3-1c67-4bf3-9f56-08d34b7133da_rg-tepmonitoring-prd-weeu-01_Microsoft.Insights_metricAlerts_MySQL_NetworkOut_GODS_PRD_-1177955119",
+    isStatefulAlert: null,
+    description: "Host Network Out: greater than 6 MB",
+    targetResource:
+      "/subscriptions/315a8af3-1c67-4bf3-9f56-08d34b7133da/resourcegroups/rg-gods-prd-weeu-01/providers/microsoft.dbformysql/flexibleservers/mysql-gods-prd-weeu-01",
+    monitorService: "Platform",
+    startDateTime: "2024-03-29T14:21:57.406Z",
+    actionStatus: { isSuppressed: false },
+    severity: "Sev2",
+    signalType: "Metric",
+    alertState: "New",
+    alertRule:
+      "/subscriptions/315a8af3-1c67-4bf3-9f56-08d34b7133da/resourcegroups/rg-tepmonitoring-prd-weeu-01/providers/Microsoft.Insights/metricAlerts/MySQL_NetworkOut_GODS_PRD",
+  },
+  EVENTID: "",
+  SUBSCRIPTION: "sub-gsotech-prd-gsoservices-04",
+  _rid: "CnpsAKUsETsBAAAAAAAAAA==",
+  _self: "dbs/CnpsAA==/colls/CnpsAKUsETs=/docs/CnpsAKUsETsBAAAAAAAAAA==/",
+  _etag: '"1c007201-0000-0d00-0000-660fd6200000"',
+  _attachments: "attachments/",
+  _ts: 1712313888,
+};
+
+export const questionAns = {};
